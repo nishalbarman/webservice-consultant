0791b17f9cdff9ff3f43b5b0354031022beedf34
Nishal Barman <helptechnoapps@gmail.com>
2023-09-29 05:55:01 +0530

    Contact us converted to react jsx

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,35 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import Contact from "./Pages/contactus/Contact";
 
 function App() {
-  const [services, setServices] = useState([]);
+  // const [services, setServices] = useState([]);
 
-  const arrayBufferToBase64 = (buffer) => {
-    var binary = "";
-    var bytes = [].slice.call(new Uint8Array(buffer));
-    bytes.forEach((b) => (binary += String.fromCharCode(b)));
-    return window.btoa(binary);
-  };
+  // const arrayBufferToBase64 = (buffer) => {
+  //   var binary = "";
+  //   var bytes = [].slice.call(new Uint8Array(buffer));
+  //   bytes.forEach((b) => (binary += String.fromCharCode(b)));
+  //   return window.btoa(binary);
+  // };
 
-  const makeRequest = async () => {
-    const res = await fetch("http://localhost:8000/services");
-    const data = await res.json();
-    setServices(data.services);
-    console.log(data);
-  };
+  // const makeRequest = async () => {
+  //   const res = await fetch("http://localhost:8000/services");
+  //   const data = await res.json();
+  //   setServices(data.services);
+  //   console.log(data);
+  // };
 
-  useEffect(() => {
-    makeRequest();
-  }, []);
+  // useEffect(() => {
+  //   makeRequest();
+  // }, []);
   return (
     <div className="App">
-      <img
+      <Contact />
+      {/* <img
         src={`data:${services[0]?.image?.mimetype};base64,${arrayBufferToBase64(
           services[0]?.image?.data.data
         )}`}
-      />
+      /> */}
     </div>
   );
 }
---
+++ client/src/Pages/contactus/Contact.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import "./Contact.css";
+
+function Contact() {
+  return (
+    <>
+      <div className="headCont">
+        <h1 className="head">Contact Us</h1>
+      </div>
+
+      <div className="container">
+        <div className="form">
+          <div className="formHeader">
+            {/* title anf ptompt here */}
+            <div className="title">
+              <h4>Let's Code Your Vision!</h4>
+            </div>
+            <div className="prompt">
+              <span>
+                Whether you've got an idea, a few technical hitches, or just
+                want to discuss tech and the future of digital, we're here. Drop
+                your messages in the box below and let's bring your visions to
+                life!
+              </span>
+            </div>
+          </div>
+
+          <div className="formBody">
+            <div className="inputCluster">
+              <div className="inputDiv">
+                <input
+                  type="text"
+                  className="inputBox"
+                  id="uName"
+                  name="uName"
+                  placeholder="Name*"
+                />
+              </div>
+              <div className="inputDiv">
+                <input
+                  type="email"
+                  className="inputBox"
+                  id="uEmail"
+                  name="uEmail"
+                  placeholder="Email*"
+                />
+              </div>
+            </div>
+            <div className="inputDiv">
+              <input
+                type="tel"
+                className="inputBox"
+                id="uPhone"
+                name="uPhone"
+                maxLength={10}
+                placeholder="Phone Number"
+              />
+            </div>
+            <div className="inputDiv">
+              <textarea
+                name=""
+                id=""
+                className="inputBox"
+                placeholder="Message*"
+              />
+            </div>
+
+            <div className="" id="">
+              <button className="btn" id="">
+                REACH US <i className="bi bi-arrow-right" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Contact;
